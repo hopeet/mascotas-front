c0739ac4b8bd66c622527707b841a10f80b04790
jose_cc <jose.ccahuana@inacapmail.cl>
2026-07-23 16:58:07 -0400

    Agrega rutas de listado y detalle de mascota

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import MascotasList from './pages/MascotasList';
+import MascotaDetail from './pages/MascotaDetail';
 import './App.css';
 
 function App() {
-  return <MascotasList />;
+  return (
+    <Routes>
+      <Route path="/" element={<MascotasList />} />
+      <Route path="/mascotas/:id" element={<MascotaDetail />} />
+    </Routes>
+  );
 }
 
 export default App;
